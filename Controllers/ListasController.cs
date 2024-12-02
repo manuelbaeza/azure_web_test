1decadf363834ba9463e2ceeddddfe5668ff8b66
Manuel <Manuel@LAPTOP-91627ORH>
2024-12-02 18:28:22 -0300

    primera subida

---
+++ Controllers/ListasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    public class ListasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
